8ef54b5b28fb4f4d3c7f07ff6ce2b8186b31e40e
Zach Beiser <zbeiser@gmail.com>
2022-11-28 10:26:54 -0800

    added license links to renderLicenseLink, completed renderLicenseSection function, added them all to genereateMarkdown function

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const questions = [
     name: 'license',
     message: 'Choose a license for your project.',
     type: 'list',
-    choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 
+    choices: ['GNU AGPL v3', 'GNU GPL v3', 'GNU LGPL v3', 'Mozilla Public License 2.0', 
     'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense', 'No License']
   },
   {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,36 +3,67 @@
 function renderLicenseBadge(license) {
   if (license === "No License") {
     return "";
-  } else if (license === "GNU AGPLv3") {
-    return "https://img.shields.io/badge/License-AGPL_v3-blue.svg"
-  } else if (license === "GNU GPLv3") {
-    return "https://img.shields.io/badge/License-GPLv3-blue.svg"
-  } else if (license === "GNU LGPLv3") {
-    return "https://img.shields.io/badge/License-LGPL_v3-blue.svg"
+  } else if (license === "GNU AGPL v3") {
+    return "https://img.shields.io/badge/License-AGPL_v3-blue.svg";
+  } else if (license === "GNU GPL v3") {
+    return "https://img.shields.io/badge/License-GPLv3-blue.svg";
+  } else if (license === "GNU LGPL v3") {
+    return "https://img.shields.io/badge/License-LGPL_v3-blue.svg";
   } else if (license === "Mozilla Public License 2.0") {
-    return "https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg"
+    return "https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg";
   } else if (license === "Apache License 2.0") {
-    return "https://img.shields.io/badge/License-Apache_2.0-blue.svg"
+    return "https://img.shields.io/badge/License-Apache_2.0-blue.svg";
   } else if (license === "MIT License") {
-    return "https://img.shields.io/badge/License-MIT-yellow.svg"
+    return "https://img.shields.io/badge/License-MIT-yellow.svg";
   } else if (license === "Boost Software License 1.0") {
-    return "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg"
+    return "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg";
   } else if (license === "The Unlicense") {
-    return "https://img.shields.io/badge/license-Unlicense-blue.svg"
+    return "https://img.shields.io/badge/license-Unlicense-blue.svg";
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "No License") {
+    return "";
+  } else if (license === "GNU AGPL v3") {
+    return "https://www.gnu.org/licenses/agpl-3.0";
+  } else if (license === "GNU GPL v3") {
+    return "https://www.gnu.org/licenses/gpl-3.0";
+  } else if (license === "GNU LGPL v3") {
+    return "https://www.gnu.org/licenses/lgpl-3.0";
+  } else if (license === "Mozilla Public License 2.0") {
+    return "https://opensource.org/licenses/MPL-2.0";
+  } else if (license === "Apache License 2.0") {
+    return "https://opensource.org/licenses/Apache-2.0";
+  } else if (license === "MIT License") {
+    return "https://opensource.org/licenses/MIT";
+  } else if (license === "Boost Software License 1.0") {
+    return "https://www.boost.org/LICENSE_1_0.txt";
+  } else if (license === "The Unlicense") {
+    return "http://unlicense.org/";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "No License") {
+    return "";
+  } else {
+    return `## License:
+    
+    ${license}
+    `;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  [![License: ${data.license}](${renderLicenseBadge(data.license)})](${renderLicenseLink(data.license)})
 
   ## Description
   
@@ -55,10 +86,6 @@ function generateMarkdown(data) {
 
   ${data.usage}
 
-  ## License
-
-  ${data.license}
-
   ## Contributing
 
   ${data.contributing}
@@ -74,6 +101,8 @@ function generateMarkdown(data) {
   GitHub: https://github.com/${data.username}
 
   Email: ${data.email}
+
+  ${renderLicenseSection(data.license)}
 `;
 }
 
